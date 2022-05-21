af7f42c4f86edadcba15c9bf39f6107cf53fd0f2
Oliver <oliver@diversionware.com.au>
2022-05-21 11:01:49 +1000

    Initial commit

---
+++ src/main/java/net/nalaisgod/wings_above/WingsAboveMod.java
@@ -0,0 +1,17 @@
+package net.nalaisgod.wings_above;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class WingsAboveMod implements ModInitializer {
+
+	public static final String MOD_ID = "wings_above";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
